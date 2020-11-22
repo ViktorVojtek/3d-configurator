4607918eb6a094a19d4744c8b6347e7589181a26
ViktorVojtek <viktor1vojtek@gmail.com>
2020-11-22 09:49:01 +0100

    Updated Menu test, destructing create from renderer of react-test-renderer

--- src/App/components/UI/Menu/Menu.test.tsx
+++ src/App/components/UI/Menu/Menu.test.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { create } from 'react-test-renderer';
 
 import Menu from './index';
 
 describe('Menu', () => {
+  const data: {
+    color?: string | undefined;
+    image?: string | undefined;
+    title: string;
+  }[] = [
+    { color: 'yellow', image: undefined, title: 'Yellow' },
+    {
+      color: 'purple',
+      image:
+        'https://images-na.ssl-images-amazon.com/images/I/A1Vmpgx%2Bg1L._AC_SL1500_.jpg',
+      title: 'Purple',
+    },
+    { color: '#007fff', title: 'Azure' },
+  ];
+
   test('snapshot renders', () => {
-    const data: {
-      color?: string | undefined;
-      image?: string | undefined;
-      title: string;
-    }[] = [
-      { color: 'yellow', image: undefined, title: 'Yellow' },
-      {
-        color: 'purple',
-        image:
-          'https://images-na.ssl-images-amazon.com/images/I/A1Vmpgx%2Bg1L._AC_SL1500_.jpg',
-        title: 'Purple',
-      },
-      { color: '#007fff', title: 'Azure' },
-    ];
-    const component = renderer.create(<Menu data={data} />);
+    const component = create(<Menu data={data} />);
 
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
